7c6220ea7bee7d30ef18be4aaed06503c2cc7a9a
Gabriel Pereira <gsilvap1994@gmail.com>
2017-04-23 03:28:22 -0300

    implementação de uma parte do front e uma parte do back

---
+++ app.js
@@ -0,0 +1,26 @@
+// libs
+const express = require('express');
+const nunjucks = require('nunjucks');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+// body-parser config
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// views config
+app.engine('html', nunjucks.render);
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'html');
+app.use(express.static(__dirname + '/public'));
+
+// config port
+app.listen(port, function () {
+    console.log('Example app listening on port 3000!');
+});
+
+require('./app/routes')(app);
+
+module.exports = app;
