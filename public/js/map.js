6a95754e694633481b29430a8ef3397d79d7d0ac
Gabriel Pereira <gsilvap1994@gmail.com>
2017-04-25 00:08:43 -0300

    segunda função implementada

--- public/js/map.js
+++ public/js/map.js
@@ -1,6 +1,7 @@
 var map;
 var geocoder;
 var infowindow;
+var filterOption;
 
 // initialize the map
 function initMap(){
@@ -19,9 +20,14 @@ $(document).ready(function(){
   initMap();
 
   $('#search').click(function(){
-    var address = $('#address').val()
-    if(address != "" && validate(address)) showInMap(address);
-    if(validate(address) == false) alert('Caracteres inválidos no endereço digitado!');
+    var address = $('#address').val();
+    var filterOption = $('select').val();
+    console.log(filterOption);
+    if (filterOption === null) {
+      $('.error-msg').val('Escolha um filtro acima');
+    }
+    else if(validate(address) == false) alert('Caracteres inválidos no endereço digitado!');
+    else if(address != "" && validate(address)) showInMap(address);
   });
 
 
@@ -41,38 +47,9 @@ function validate(address) {
 // show the address
 function showInMap(address) {
   geocoder.geocode({ 'address': address + ', Brasil', 'region': 'BR' }, function(results, status){
-
     if(status == google.maps.GeocoderStatus.OK) {
       if(results[0]) {
-        var lat = results[0].geometry.location.lat();
-        var lng = results[0].geometry.location.lng();
-
-        $('#address').val(results[0].formatted_address);
-
-        var location = new google.maps.LatLng(lat, lng);
-
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-
-        var request = {
-          location: location,
-          radius: 5000,
-          type: 'car_repair',
-          keyword: 'oficina',
-          name: 'mecanica'
-        }
-        service.nearbySearch(request, callback);
-
-        var marker = new google.maps.Marker({
-          map: map,
-          position: location
-        });
-        marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
-        map.setCenter(location);
-        map.setZoom(12);
-        infowindow.setContent('Você está aqui!');
-        infowindow.open(map, marker);
-
+        makeSearch(results);
       }
     }
     else {
@@ -81,26 +58,67 @@ function showInMap(address) {
   });
 }
 
+function makeSearch(results) {
+  if(results[0]) {
+    var lat = results[0].geometry.location.lat();
+    var lng = results[0].geometry.location.lng();
+
+    $('#address').val(results[0].formatted_address);
+
+    var location = new google.maps.LatLng(lat, lng);
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+
+    var request = {
+      location: location,
+      radius: 5000,
+      type: 'car_repair',
+      keyword: 'oficina',
+      name: 'mecanica'
+    }
+    service.nearbySearch(request, callback);
+    var marker = new google.maps.Marker({
+      map: map,
+      position: location
+    });
+    marker.setIcon('http://maps.google.com/mapfiles/ms/icons/green-dot.png');
+    map.setCenter(location);
+    map.setZoom(12);
+    infowindow.setContent('Você está aqui!');
+    infowindow.open(map, marker);
+
+  }
+}
+
 function callback(results, status) {
   var service = new google.maps.places.PlacesService(map);
-
   if (status === google.maps.places.PlacesServiceStatus.OK){
     for(var i = 0; i < results.length; i++) {
       place = results[i];
-      console.log(results.length);
        service.getDetails({ placeId : place.place_id}, function (_place, status){
-          if(status === google.maps.places.PlacesServiceStatus.OK) {
-            createMarker(_place);
-            generateTable(_place);
+          if (status === google.maps.places.PlacesServiceStatus.OK) {
+            console.log("quero ver a option aqui: "+filterOption);
+            if (filterOption == 1) {
+              createMarker(_place);
+              generateTable(_place);
+            }
+            if(filterOption == 2) {
+              if(_place.rating != undefined && _place.rating >= 4){
+                console.log(_place.name+' Avaliação: '+_place.rating);
+                createMarker(_place);
+              }
+            }
+
           }
-       })
+       });
     }
   }
 }
 
+
 function createMarker(place) {
 
-  console.log(place)
   var placeLoc = place.geometry.location;
   var marker = new google.maps.Marker({
     map: map,
@@ -115,6 +133,8 @@ function createMarker(place) {
     infowindow.open(map, this);
   });
 }
+
+
 
 // criar tabela para mostrar todas as informações desejadas para poder mudar o método de organização
 
